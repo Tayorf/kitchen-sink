af4df63a52b4b762d21b9c66cb69c5a3bbc1888a
taylor fowler <taylorfowler77@icloud.com>
2023-09-25 22:00:50 -0400

    steps 4 through 8

---
+++ app.js
@@ -0,0 +1,12 @@
+var firstName = 'Taylor';
+console.log(firstName);
+const usStates =  50;
+console.log(usStates);
+let add = 5 + 4;
+console.log(add);
+// practicing addition!!
+function sayHello(HelloWorld){
+    alert('HelloWorld');
+};
+sayHello();
+
