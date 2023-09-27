7a29ec7d9bcdc829dd05207fbae9d487ba7ee503
taylor fowler <taylorfowler77@icloud.com>
2023-09-26 20:05:17 -0400

    steps 9 through 11

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var firstName = 'Taylor';
 console.log(firstName);
-const usStates =  50;
+const usStates = 50;
 console.log(usStates);
 let add = 5 + 4;
 console.log(add);
 // practicing addition!!
-function sayHello(HelloWorld){
+function sayHello(HelloWorld) {
     alert('HelloWorld');
 };
 sayHello();
-
+function checkAge(name, age) {
+    if (age < 21) {
+        alert("Sorry " + name + " , you aren't old enough to view this page!");
+    }
+};
+let output1 = checkAge("Charles", 21);
+let output2 = checkAge("Abby", 27);
+let output3 = checkAge("James", 18);
+let output4 = checkAge("John", 17);
+checkAge();
+var veggies = ['carrot', 'bean', 'potato', 'squash']
+for (var i = 0; i < veggies.length; i++) {
+console.log(veggies[i]);
+};
+let pet={
+    name:'teddy',
+    breed:'doodle'
+};
+console.log(pet);
